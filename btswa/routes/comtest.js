d177f6b3cd713715779cc9ee5663ba4da7d11ec9
liyunhan <liyunhan@alibaba-inc.com>
2018-01-10 01:48:46 +0800

    change seed

--- btswa/routes/comtest.js
+++ btswa/routes/comtest.js
@@ -3,38 +3,18 @@ var express = require('express');
 var router = express.Router();
 var CompermUtil=require('../utils/compermutil');
 var BtspwdCheck=require('../utils/btspwdcheck');
-var binfilePath = '/res/bts_default_20170914.bin';
+const binfilePath = '/Users/liyunhan/Documents/bts_default.bin';
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     addNewPwdTask(res);
-    // res.send("yes");
+    res.send("yes");
 });
 
 module.exports = router;
-var test1 = [
-    "Liyh",
-    "0620",
-    "Play",
-    "$$",
-    "o"
-];
 
 function addNewPwdTask(response) {
-    var comperm=Object.create(CompermUtil);
-    comperm.init(test1,test1.length,3);
+
     var btspwdcheck=Object.create(BtspwdCheck);
     btspwdcheck.init(binfilePath);
-    comperm.getEach(function (permArr) {
-        let pwd=permArr.join('');
-        let obj= btspwdcheck.decryptBts(pwd);
-        console.log(obj.code+":"+pwd);
-        if(obj.code==0){
-            comperm.result=pwd;
-            comperm.canloop=false;
-            console.log(comperm.getinfo());
-            response.send(comperm.getinfo());
-        }
-
-    });
-    response.send(comperm.getinfo());
+    btspwdcheck.decryptBts("Liyh0620$$god");
 }
